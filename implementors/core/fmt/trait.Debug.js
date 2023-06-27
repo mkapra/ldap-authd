e0765883f0ca23843d0b0783702e972ca5290a44
mkapra <mkapra@users.noreply.github.com>
2023-06-27 06:27:56 +0000

    deploy: 4228aa8074efc3467b4043a04b8fb9a19ba723e2

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ldap_authd":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"ldap_authd/struct.Cli.html\" title=\"struct ldap_authd::Cli\">Cli</a>"]]
+"ldap_authd":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"ldap_authd/struct.Cli.html\" title=\"struct ldap_authd::Cli\">Cli</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
